4940a2e882769b0eea1423313bdfe4bdb3b8354a
Commandant325 <artusdechavagnac@gmail.com>
2023-11-24 14:48:50 +0100

    1re MAJ

---
+++ script.js
@@ -0,0 +1,25 @@
+function seConnecter() {
+
+	top.location.href = "connexion.html";
+
+}
+
+// function monCompte() {
+
+// 	// 
+
+// }
+
+// function connexion() {
+	
+// 	sessionStorage.setItem("connecte", true);
+
+// }
+
+// function function_name() {
+
+// 	if (sessionStorage.getItem("connecte") == true) {
+// 		document.getElementById("span-se-connecter").style.display = "none";
+// 	}
+
+// }
