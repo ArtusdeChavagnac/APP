d58dd30ab3a4c7deb22041817e21df8effd9ecd5
Artus de Chavagnac <artus2003chavagnac@gmail.com>
2024-03-01 12:20:18 +0100

    maj

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-function submitconfirm(motDePasse,email,form){
-                
-    var mail = email;
-    //var mail = document.forms["fcontact"]["email"].value;
-    var mdp = motDePasse;
-    if (mdp.length < 8)
-    {
-        alert("Votre mot de passe est trop court");
-        return false;
-    }
-    atpos= mail.indexOf("@");
-    dotpos = mail.indexOf(".");
-    else if (atpos<1 ||(dotpos - atpos<2))
-    {
-        alert("Entrez un mail valide svp");
-        return false;
-    }
-    else{
-    	form.submit();
-    	return true;
-    }
+function submitconfirm(motDePasse, email, form) {
+
+	var mail = email;
+	//var mail = document.forms["fcontact"]["email"].value;
+	var mdp = motDePasse;
+	if (mdp.length < 8) {
+		alert("Votre mot de passe est trop court");
+		return false;
+	}
+	atpos = mail.indexOf("@");
+	dotpos = mail.indexOf(".");
+	else if (atpos < 1 || (dotpos - atpos < 2)) {
+		alert("Entrez un mail valide svp");
+		return false;
+	} else {
+		form.submit();
+		return true;
+	}
+
 }
